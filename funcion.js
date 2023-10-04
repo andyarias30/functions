8800fb7fc6cebf43b8e601d920cbd2141eda8b4e
andyarias30 <146486695+andyarias30@users.noreply.github.com>
2023-10-04 16:32:09 -0400

    made some changes

--- funcion.js
+++ funcion.js
@@ -1,7 +1,9 @@
 function returnSomething(){ //crate a new function
     return 5;  // return number 5
 }
- returnSomething()
+ 
+returnSomething()
   
  const value = returnSomething() //call the function, and store the value
  console.log(value)
+    
--- project.js
+++ project.js
@@ -6,8 +6,9 @@ console.log(fahrenheit)
 temperartureConvertion(32)
 
 // // another way 
-// function celsiusToFahrenheit(celsius){
-//     const temperatureInFahernheit = celsius * 9 / 5 + 32
-//     return temperatureInFahernheit
-// }
-// console.log(celsiusToFahrenheit(32) + '°F ')
+function celsiusToFahrenheit(celsius){
+    const temperatureInFahernheit = celsius * 9 / 5 + 32
+    return temperatureInFahernheit
+ }
+ console.log(celsiusToFahrenheit(32) + '°F ')
+ 
